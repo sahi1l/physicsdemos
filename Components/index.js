236881c1f0d258fbf123bb797d6ff1b788ea6ae6
Sam Hill <physhill@gmail.com>
2023-08-23 15:00:37 -0400

    Rewrote Components to use Snap instead of Raphael

--- Components/index.js
+++ Components/index.js
@@ -1,13 +1,16 @@
+/*global Snap,$ */
 import {Score} from "../lib/quiz.js";
 import {Help} from "../lib/default.js";
 //FIX: Add +x and +y axes
-var guess={x:1,y:1,trig:false};
-var correct={x:1,y:1,trig:false};
+var guess={x:1,y:1,vertical:false};
+var correct={x:1,y:1,vertical:false};
+const vlength = 40;
 var timeout;
 //var sign={x:1,y:1};
-//var trig=false;
-function SetMagnitude(val){$(".magnitude").each(function(i,e){e.innerHTML=val;});};
-function SetAngle(val){$(".angle").each(function(i,e){e.innerHTML=val;});};
+//var vertical=false;
+let fadedColor = "#aaa";
+function SetMagnitude(val){$(".magnitude").each(function(i,e){e.innerHTML=val;});}
+function SetAngle(val){$(".angle").each(function(i,e){e.innerHTML=val;});}
 function ToggleSign(which){
     guess[which]=-guess[which];
     var c;
@@ -15,53 +18,119 @@ function ToggleSign(which){
     $("#"+which).html(c);
 }
 function ToggleTrig(){
-    guess.trig=!guess.trig;
-    var x="cos"; var y="sin";
-    if(guess.trig){
+    guess.vertical=!guess.vertical;
+    let x="cos";
+    let y="sin";
+    if(guess.vertical){//trig means vertical
         x="sin"; y="cos";
     }
     $("#xtrig").html(x); $("#ytrig").html(y);
 }
-var paper=undefined,base,vector,Lang,arc,correctQ;
-function CalcComponents(obj,angle,trig){
-    var radians=angle*Math.PI/180.0;
-    if(trig){
-        return {x:obj.x*Math.sin(radians),
-                y:obj.y*Math.cos(radians)};
-    } else {
-        return {x:obj.x*Math.cos(radians),
-                y:obj.y*Math.sin(radians)};
+var paper=undefined,base,vector,AngleText,arc,correctQ,circle,mask,arrow;
+class Arrow {
+    constructor(paper,x1,y1,x2,y2,attrs={}) {
+        let sc = 2; //scale of arrowhead
+        this.color = attrs.color??"grey";
+        this.width = attrs.width??3;
+        this.class = attrs.class??"";
+        this.obj = paper.group();
+        this.headid = "head" + parseInt(1e6*Math.random());
+        this.arrowhead=paper.polyline(0,-sc,2*sc,0,0,sc).attr({fill:this.color,class:this.class});
+        this.arrowhead.marker(0,-sc,2*sc,2*sc,sc,0).attr({id:this.headid});
+        this.line = paper.line(x1,y1,x2,y2)
+            .attr({"stroke-width":this.width,
+                   "stroke-linecap":"round",
+                   "stroke": this.color,
+                   "class": this.class
+                  });
+        this.obj.append(this.line);
+        this.line.node.style['marker-end'] = Snap.url(this.headid);
+        console.debug(this.line);
+    }
+    attr(args) {
+        this.line.attr(args);
+    }
+    setColor(color) {
+        this.line.node.style["stroke"]=color;
+        this.arrowhead.node.style["fill"] = color;
     }
 }
-function DrawArrow(xs,ys,trig,angle){
-    //xs and ys are +/-1, trig works as above, angle is the angle in degrees
-    //this should fix the base, the vector, and the angle indicator
-    //positioning the angle indicator may be difficult
-    correct={x:xs,y:ys,trig:trig};
-    SetAngle(angle);
-    var full = CalcComponents({x:xs,y:-ys},angle,trig);
-    var half = CalcComponents({x:xs,y:-ys},0.5*angle,trig);
-    var zero = CalcComponents({x:xs,y:-ys},0,trig);
-    if(trig){
-        base.attr("path","M0,-50L0,50");
-    } else {
-        base.attr("path","M-50,0L50,0");
+class LabelledArrow {
+    /*An arrow with a label and a dotted arc between angle (0-360) and horizontal or vertical*/
+    constructor(paper,length,angle,vertical) {
+        this.color = "grey";
+        this.paper = paper;
+        this.length = length;
+
+        
+        this.mask = paper.group();
+        this.mask.rect = paper.polyline(0,0,0,0).attr({fill:"black"});
+        this.mask.wedge = paper.polyline(0,0,0,0).attr({fill:"white"});
+        this.mask.append(this.mask.rect);
+        this.mask.append(this.mask.wedge);
+        
+        this.circle = paper.circle(0,0,0.8*length)
+            .attr({"stroke-dasharray": "1,1",
+                   "fill": "transparent",
+                   "stroke": "grey",
+                   "mask": this.mask
+                  });
+        this.vector = new Arrow(paper,0,0,0,0);
+/*        this.vector = paper.line(0,0,0,0)
+            .attr({"stroke-width":3,
+                   "stroke-linecap":"round",
+                   "stroke":this.color});
+*/
+        this.label = paper.text(0,0,"?°")
+            .attr({"font-size":parseInt(length/6), "text-anchor": "middle", "dominant-baseline":"middle"});
+
+        
+        if(angle!=undefined) {this.SetAngle(angle,vertical);}
+        this.vector.setColor("var(--main-color)");
     }
-    vector.attr("path",`M0,0L${50*full.x},${50*full.y}`);
-    var llength=-0.43*angle+48.6;
-    Lang.attr({x:llength*half.x,
-               y:llength*half.y,
-               text:angle+"°"});
-    llength*=0.8;
-    arc.attr({path:`M${llength*zero.x},${llength*zero.y},\
-                    S${llength*half.x},${llength*half.y},\
-                    ${llength*full.x},${llength*full.y}`}
-            );
+
+    Components(xs,ys,angle,vertical) {
+        let radians=angle*Math.PI/180.0;
+        if(vertical){
+            return {x:xs*Math.sin(radians),
+                    y:ys*Math.cos(radians)};
+        } else {
+            return {x:xs*Math.cos(radians),
+                    y:ys*Math.sin(radians)};
+        }
+    }
+    
+    SetAngle(xs,ys,vertical,angle) {//FIX: Maybe figure out xs/ys separately...later. Bigger nightmare than I thought.
+
+        let XS = xs*this.length;
+        let YS = -ys*this.length;
+        let full = this.Components(XS, YS, angle, vertical);
+        let half = this.Components(XS, YS, 0.5*angle, vertical);
+        let zero = this.Components(XS, YS, 0, vertical);
+        if(vertical) {
+            base.attr({x1:0, y1: -this.length, x2: 0, y2: this.length});
+        } else {
+            base.attr({x1: -this.length, y1:0, x2: this.length, y2: 0});
+        }
+        this.vector.attr({x2: full.x, y2: full.y});
+        this.label.attr({x: 0.6*half.x,
+                         y: 0.6*half.y,
+                         text: angle+"°"});
+        let M = Math.max(Math.abs(full.x/full.y),Math.abs(full.y/full.x));
+        let X = 20*M*full.x; let Y = 20*M*full.y;
+
+        this.mask.rect.attr("points", [0,0,X,0,X,Y,0,Y].join(","));
+        if(vertical) {
+            this.mask.wedge.attr("points", [0,0,X,Y,0,Y,0,0].join(","));
+        } else {
+            this.mask.wedge.attr("points", [0,0,X,Y,X,0,0,0].join(","));
+        }
+    }
 }
 function CheckAnswer(){
     //Is the answer correct?
-    if(guess.x==correct.x && guess.y==correct.y && guess.trig==correct.trig){
-        correctQ.attr({text:"Correct!",fill:"green",opacity:1});
+    if(guess.x==correct.x && guess.y==correct.y && guess.vertical==correct.vertical){
+        correctQ.attr({text:"Correct!",fill:"var(--right-color)",opacity:1});
         score.correct++;
         score.number++;
         score.total++;
@@ -72,8 +141,8 @@ function CheckAnswer(){
         }
     } else {
         score.total++; score.error++;
-        correctQ.attr({text:"Try again",fill:"red",opacity:1});
-        correctQ.animate({opacity:0},2000,ClearCorrect);
+        correctQ.attr({text:"Try again",fill:"var(--wrong-color)",opacity:1});
+        correctQ.animate({opacity:0},2000);
     }
     updateScore();
 }
@@ -92,23 +161,42 @@ function updateNumber(){
 }
 function RandomVector(){
     updateNumber();
-    DrawArrow(
-        Math.floor(Math.random()*2)*2-1,
-        Math.floor(Math.random()*2)*2-1,
-        Math.floor(Math.random()*2),
-        Math.floor(Math.random()*13)*5+20
-        );
-    
+    let xs,ys,vertical;
+    do {
+        xs = Math.floor(Math.random()*2)*2-1;
+        ys = Math.floor(Math.random()*2)*2-1;
+        vertical = Math.floor(Math.random()*2);
+    } while (xs==correct.x && ys==correct.y && vertical==correct.vertical);
+    let angle = Math.floor(Math.random()*13)*5+20;
+//    [xs,ys,vertical] = [1,1,0];
+
+    correct = {x:xs, y:ys, vertical: vertical};
+    console.debug("correct: ",correct);
+    arrow.SetAngle(xs,ys,vertical, angle);
+    $(".angle").each(function(i,e){e.innerHTML=angle;});
 }
 var score={correct:0,total:0,number:1,max:10,error:0};
 function init() {
     if(paper!=undefined){paper.remove();}
-    paper=Raphael("canvas","100%","100%");
-    paper.setViewBox(-50,-50,100,100);
-    base=paper.path("M0,0L30,0").attr({"stroke-dasharray":"-"});
-    arc=paper.path("").attr({"stroke-dasharray":".",stroke:"grey","stroke-width":1});
-    vector=paper.path("M0,0L10,30").attr({"stroke-width":3,"arrow-end":"classic","stroke-linecap":"round","stroke":"grey"});
-    Lang=paper.text(0,0,"20°").attr({"font-size":8});
+    paper = Snap("#canvas");
+    let size = vlength+10;
+    paper.attr({viewBox:[-size,-size,2*size,2*size].join(",")});
+    //basis
+    let basis = {size: 10};
+    basis.cx = size-basis.size-10;
+    basis.cy = 2*basis.size-size+5;
+    basis.x = new Arrow(paper,basis.cx,basis.cy,basis.cx+basis.size,basis.cy,{width:1,color:fadedColor});
+    basis.y = new Arrow(paper,basis.cx,basis.cy,basis.cx,basis.cy-basis.size,{width:1,color:fadedColor});
+    let font = {"font-size":size/10,"text-anchor":"middle","dominant-baseline":"middle",fill:fadedColor};
+    basis.tx = paper.text(basis.cx+basis.size+5,basis.cy,"+x").attr(font);
+    basis.ty = paper.text(basis.cx,basis.cy-basis.size-5,"+y").attr(font);
+    
+    base=paper.line(0,0,30,0).attr(
+        {stroke:"black",
+         "stroke-dasharray": "1,1"
+        });
+    let vectorcolor = "grey";
+    arrow = new LabelledArrow(paper,vlength);
     correctQ=paper.text(-50,43,"")
         .attr({"font-size":10,"text-anchor":"start",opacity:0});
     $(".trig").click(ToggleTrig);
@@ -116,9 +204,9 @@ function init() {
     $("#y").click(function(){ToggleSign("y")});
     $("#check").click(CheckAnswer);
     new Help($("#help"));
-    RandomVector();
-    updateScore();
-}
+    start();
+    }
+
 function start(){
     score.correct=0;
     score.total=0;
